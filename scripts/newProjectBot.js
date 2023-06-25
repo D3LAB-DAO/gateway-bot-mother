e0b6636dfa4679e4b51148a21e9a1d59a9f263b1
solidity12 <volibear1001@gmail.com>
2023-06-26 01:17:54 +0900

    feat: Updated message

--- scripts/newProjectBot.js
+++ scripts/newProjectBot.js
@@ -7,16 +7,21 @@ const bot = new TelegramBot(token, { polling: true });
 
 let expectingUrl = false;
 
-bot.onText(/\/help/, (msg) => {
-    const helpMessage = `
-Here are the commands you can use:
-/newProject - enter this command, then input a URL when prompted.
+bot.onText(/\/start/, (msg) => {
+    const chatId = msg.chat.id;
+    const response = `
+Hello! I'm your V8S bot.
+I can help you create new projects. Here is the command you can use:
+
+/newProject - start a new project and I will guide you through the process
+
+Just type /newProject to get started!
 `;
-    bot.sendMessage(msg.chat.id, helpMessage);
+    bot.sendMessage(chatId, response);
 });
 
 bot.onText(/\/newProject/, (msg) => {
-    bot.sendMessage(msg.chat.id, 'Please input the URL.');
+    bot.sendMessage(msg.chat.id, 'Please input the URL for the new project.');
     expectingUrl = true;
 });
 
@@ -25,7 +30,8 @@ bot.on('message', (msg) => {
         // Add code to handle the URL here. For example, save the URL to a database.
         // const url = msg.text;
 
-        bot.sendMessage(msg.chat.id, 'The process has been completed.');
+        bot.sendMessage(msg.chat.id, `Done. Your project has been newly registered in the "0x" V8S contract. Thank you! For questions, please contact lukepark@mg12.com.`);
+
         expectingUrl = false;
     }
 });
